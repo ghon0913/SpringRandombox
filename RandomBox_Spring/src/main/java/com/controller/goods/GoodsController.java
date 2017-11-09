f5d127d7565a993d8010b866a7e52d619e22f804
acorn <acorn@acorn-PC>
2017-11-09 20:31:38 +0900

    dd

--- RandomBox_Spring/src/main/java/com/controller/goods/GoodsController.java
+++ RandomBox_Spring/src/main/java/com/controller/goods/GoodsController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -233,4 +234,12 @@ public class GoodsController {
 
 		return mav;
 	}
+	
+	@RequestMapping("/deleteGoods")
+	public @ResponseBody String deleteGoods(@RequestParam String gCode) {
+		
+		service.deleteGoods(gCode);
+		return gCode;
+	}
+		
 }
